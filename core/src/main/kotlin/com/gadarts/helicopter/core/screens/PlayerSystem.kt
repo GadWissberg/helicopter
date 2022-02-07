b333560dc6de40a85ac8a64ecac252cde7b6ab4c
Gad Wissberg <gadw17@gmail.com>
2022-02-07 08:56:49 +0200

    Adding player movement.

--- core/src/main/kotlin/com/gadarts/helicopter/core/screens/PlayerSystem.kt
+++ core/src/main/kotlin/com/gadarts/helicopter/core/screens/PlayerSystem.kt
@@ -3,7 +3,6 @@ package com.gadarts.helicopter.core.screens
 import com.badlogic.ashley.core.Engine
 import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.core.PooledEngine
-import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.g3d.ModelInstance
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.math.Vector3
@@ -19,6 +18,8 @@ import com.gadarts.helicopter.core.components.child.ChildModel
 import com.gadarts.helicopter.core.systems.GameEntitySystem
 import com.gadarts.helicopter.core.systems.SystemsData
 import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 
 class PlayerSystem(private val data: SystemsData, private val assetsManager: GameAssetManager) :
@@ -49,35 +50,45 @@ class PlayerSystem(private val data: SystemsData, private val assetsManager: Gam
     private fun updateDesiredDirection(deltaX: Float, deltaY: Float) {
         desiredDirection.set(deltaX, deltaY)
         desiredDirectionChanged = true
+        updateRotationStep()
+    }
+
+    private fun updateRotationStep() {
         val diff = desiredDirection.angle() - currentDirection.angle()
-        rotation = if (auxVector2.set(1F, 0F).setAngle(diff).angle() > 180) {
-            -1F
+        val negativeRotation = auxVector2.set(1F, 0F).setAngle(diff).angle() > 180
+        if (negativeRotation && rotation < 0) {
+            rotation = max(rotation - ROTATION_INCREASE, -MAX_ROTATION_STEP)
+        } else if (!negativeRotation && rotation > 0) {
+            rotation = min(rotation + ROTATION_INCREASE, MAX_ROTATION_STEP)
         } else {
-            1F
+            rotation = INITIAL_ROTATION_STEP * (if (negativeRotation) -1F else 1F)
         }
     }
 
     override fun update(deltaTime: Float) {
         super.update(deltaTime)
-        handleRotation()
+        handleRotation(deltaTime)
     }
 
-    private fun handleRotation() {
+    private fun handleRotation(deltaTime: Float) {
         if (desiredDirectionChanged) {
-            Gdx.app.log("current", "${currentDirection.angle()}")
-            Gdx.app.log("desired", "${desiredDirection.angle()}")
-            if (abs(currentDirection.angle() - desiredDirection.angle()) > 0.5F) {
-                rotate()
+            val rotBefore = rotation
+            updateRotationStep()
+            val didNotReachYet = (rotBefore < 0 && rotation < 0) || (rotBefore > 0 && rotation > 0)
+            val diff = abs(currentDirection.angle() - desiredDirection.angle())
+            if (didNotReachYet && diff > ROTATION_EPSILON
+            ) {
+                rotate(deltaTime)
             } else {
                 desiredDirectionChanged = false
             }
         }
     }
 
-    private fun rotate() {
+    private fun rotate(deltaTime: Float) {
         val transform = ComponentsMapper.modelInstance.get(player).modelInstance.transform
         val position = transform.getTranslation(auxVector3)
-        currentDirection.rotate(rotation)
+        currentDirection.rotate(rotation * deltaTime)
         transform.setToRotation(Vector3.Y, currentDirection.angle())
         transform.setTranslation(position)
     }
@@ -87,7 +98,7 @@ class PlayerSystem(private val data: SystemsData, private val assetsManager: Gam
         return EntityBuilder.begin()
             .addModelInstanceComponent(
                 assetsManager.getModel(ModelsDefinitions.APACHE),
-                auxVector3.set(2F, 2F, 2F)
+                auxVector3.set(0F, 2F, 2F)
             ).addChildModelInstanceComponent(
                 listOf(
                     ChildModel(
@@ -98,16 +109,19 @@ class PlayerSystem(private val data: SystemsData, private val assetsManager: Gam
                 ),
                 true
             ).addAmbSoundComponent(assetsManager.getSound(SfxDefinitions.PROPELLER))
-            .addCharacterComponent(PLAYER_INITIAL_HP)
+            .addCharacterComponent(INITIAL_HP)
             .addPlayerComponent()
             .finishAndAddToEngine()
     }
 
     companion object {
-        private const val PLAYER_INITIAL_HP = 100
-        private const val PLAYER_INITIAL_FUEL = 100
+        private const val INITIAL_HP = 100
+        private const val INITIAL_FUEL = 100
+        private const val MAX_ROTATION_STEP = 200F
+        private const val ROTATION_INCREASE = 2F
+        private const val INITIAL_ROTATION_STEP = 6F
         val auxVector3 = Vector3()
         val auxVector2 = Vector2()
-
+        private const val ROTATION_EPSILON = 0.5F
     }
 }
