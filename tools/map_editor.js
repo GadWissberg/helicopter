7f8d3ae957ade0078029ca03865454e82e3c5c17
gad.wissberg <gad.wissberg@viaccess-orca.com>
2022-03-17 14:33:32 +0200

    Working on map.

--- tools/map_editor.js
+++ tools/map_editor.js
@@ -1,14 +1,43 @@
 const DEFAULT_SIZE = 48
 const ID_MAP = "map"
+const Modes = Object.freeze({
+    TILES:   Symbol("tiles"),
+    OBJECTS:  Symbol("objects")
+});
+const body = document.body;
+const table = document.getElementById(ID_MAP).appendChild(document.createElement('table'));
+const MODES_GROUP_NAME = "modes"
+class CellData {
+}
+
+function onCellClicked(row, col){
+    var cell = table.rows[row].cells[col]
+        if (cell.cellData == null){
+            cell.cellData = new CellData()
+        }
+    }
+    var cellData = cell.cellData;
+    selectedMode = document.querySelector('input[name="'+MODES_GROUP_NAME+'"]:checked').value
+    if (selectedMode == Modes.TILES){
+        cellData.tile = "#"
+        alert("tiles")
+    } else if (selectedMode == Modes.OBJECTS){
+        cellData.tile = "0"
+        alert("objects")
+    }
+    alert("row:" +row +", col:" + col)
+}
+
 function tableCreate() {
-  const body = document.body;
-  tbl = document.getElementById(ID_MAP).appendChild(document.createElement('table'));
 
   for (let i = 0; i < DEFAULT_SIZE; i++) {
-    const tr = tbl.insertRow();
+    const tr = table.insertRow();
     for (let j = 0; j < DEFAULT_SIZE; j++) {
         const td = tr.insertCell();
 //        td.appendChild(document.createTextNode("&nbsp;"));
+        td.addEventListener('click', e=> {
+            onCellClicked(i, j)
+        })
     }
   }
 
