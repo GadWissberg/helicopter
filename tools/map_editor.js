20b2ca690f7b3f885989f9b33fce9fd454db984a
Gad Wissberg <gadw17@gmail.com>
2022-03-24 23:04:05 +0200

    Working on map.

--- tools/map_editor.js
+++ tools/map_editor.js
@@ -24,8 +24,8 @@ const ElementsDefinitions = Object.freeze([
     "GUARD_HOUSE",
     "ANTENNA",
 ]);
-const TILE_SAND = "#886A08"
-const TILE_ROAD = "#424242"
+const TILE_SAND = Object.freeze({ tile: "#886A08", symbol: "0" })
+const TILE_ROAD = Object.freeze({ tile: "#424242", symbol: "1" });
 const body = document.body;
 const table = document.getElementById(ID_MAP).appendChild(document.createElement('table'));
 const RADIO_GROUP_NAME_MODES = "modes"
@@ -40,10 +40,12 @@ const OUTPUT_FILE_NAME = 'map.json';
 class MapEditor {
 
     constructor() {
-        this.tableCreate();
+        this.createTable();
+        this.initializeModesRadioButtons();
         this.inflateElementsLeftMenu();
         table.style.width = DEFAULT_SIZE * 64 + 'px';
         table.style.height = DEFAULT_SIZE * 64 + 'px';
+        var self = this;
         document.getElementById(DIV_ID_BUTTON_SAVE).addEventListener('click', e => {
             var output = {};
             var tilesString = calculateTilesMapString();
@@ -77,13 +79,14 @@ class MapEditor {
 
             function calculateTilesMapString() {
                 var tilesString = "";
-                for (var row in table.rows) {
-                    for (var cell in table.rows[row].cells) {
+                for (var row = 0; row < table.rows.length; row++) {
+                    for (var col = 0; col < table.rows[row].cells.length; col++) {
                         var currentTile = TILE_SAND;
-                        if (table.rows[row].cells[cell].cellData != null) {
-                            currentTile = table.rows[row].cells[cell].cellData.tile;
+                        var cellData = table.rows[row].cells[col].cellData;
+                        if (cellData != null && cellData.tile != null) {
+                            currentTile = self.findTileBySymbol(cellData.tile.symbol);
                         }
-                        tilesString += (currentTile == TILE_ROAD ? '1' : '0');
+                        tilesString += currentTile.symbol;
                     }
                 }
                 return tilesString;
@@ -100,16 +103,47 @@ class MapEditor {
 
         });
 
-
-        document.getElementById(DIV_ID_BUTTON_LOAD).addEventListener('click', e => {
+        document.getElementById(DIV_ID_BUTTON_LOAD).addEventListener('click', () => {
 
             var input = document.createElement('input');
             input.type = 'file';
+            input.addEventListener('change', e => {
+                var file = e.target.files[0];
+                if (!file) {
+                    return;
+                }
+                var reader = new FileReader();
+                var self = this;
+                reader.onload = e => {
+                    self.createTable();
+                    var contents = e.target.result;
+                    var inputMapObject = JSON.parse(contents);
+                    for (var i = 0; i < inputMapObject.tiles.length; i++) {
+                        var cell = table.rows[Math.floor(i / DEFAULT_SIZE)].cells[i % DEFAULT_SIZE];
+                        var placedTile = self.findTileBySymbol(inputMapObject.tiles.charAt(i));
+                        cell.style.backgroundColor = placedTile.tile;
+                        cell.cellData=new CellData();
+                        cell.cellData.tile = placedTile;
+                    }
+                    for (var i = 0; i < inputMapObject.elements.length; i++) {
+                        var element = inputMapObject.elements[i];
+                        self.placeElementObject(table.rows[element.row].cells[element.col], element.definition);
+                    }
+                };
+                reader.readAsText(file);
+            }, false);
             input.click();
         });
 
     }
 
+    findTileBySymbol(symbol) {
+        var result = TILE_SAND;
+        if (symbol == TILE_ROAD.symbol) {
+            result = TILE_ROAD;
+        }
+        return result;
+    }
     inflateElementsLeftMenu() {
         var leftMenu = document.getElementById(DIV_ID_LEFT_MENU);
         ElementsDefinitions.forEach(element => {
@@ -148,12 +182,18 @@ class MapEditor {
     }
 
     placeElementInCell(cell, cellData, selectedMode, input) {
+        var self = this;
         var leftClick = input == "click";
         if (selectedMode == Modes.TILES) {
             applyTileChangeInCell(leftClick, cellData, cell);
         } else if (selectedMode == Modes.OBJECTS) {
+            applyElementChangeInCell();
+        }
+
+        function applyElementChangeInCell() {
             if (leftClick) {
-                placeElementObject(this, cell);
+                var selection = document.querySelector('input[name="' + RADIO_GROUP_NAME_GAME_OBJECT_SELECTIONS + '"]:checked').value;
+                self.placeElementObject(cell, selection);
             } else {
                 removeElementObject(editor, cell);
             }
@@ -162,21 +202,24 @@ class MapEditor {
         function applyTileChangeInCell(leftClick, cellData, cell) {
             var selectedTile = leftClick ? TILE_ROAD : TILE_SAND;
             cellData.tile = selectedTile;
-            cell.style.backgroundColor = selectedTile;
+            cell.style.backgroundColor = selectedTile.tile;
         }
 
-        function placeElementObject(editor, cell) {
-            var textNode = editor.getOrAddChildTextNode(cell);
-            var selection = document.querySelector('input[name="' + RADIO_GROUP_NAME_GAME_OBJECT_SELECTIONS + '"]:checked').value;
-            cellData.object = selection;
-            textNode.nodeValue = selection;
-        }
 
         function removeElementObject(editor, cell) {
             var textNode = editor.getOrAddChildTextNode(cell);
             cellData.object = null;
             textNode.nodeValue = null;
         }
+    }
+
+    placeElementObject(cell, selection) {
+        var textNode = this.getOrAddChildTextNode(cell);
+        if (cell.cellData == null) {
+            cell.cellData = new CellData();
+        }
+        cell.cellData.object = selection;
+        textNode.nodeValue = selection;
     }
 
     onCellLeftClicked(row, col) {
@@ -204,7 +247,8 @@ class MapEditor {
         return textNode
     }
 
-    tableCreate() {
+    createTable() {
+        table.innerHTML = "";
         for (let i = 0; i < DEFAULT_SIZE; i++) {
             const tr = table.insertRow();
             for (let j = 0; j < DEFAULT_SIZE; j++) {
@@ -219,7 +263,6 @@ class MapEditor {
                 }, false)
             }
         }
-        this.initializeModesRadioButtons();
     }
 
     initializeModesRadioButtons() {
