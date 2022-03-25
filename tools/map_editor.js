fda379dec4c5c7bed60aa1f12758abce5207db56
Gad Wissberg <gadw17@gmail.com>
2022-03-25 14:27:38 +0300

    Working on map.

--- tools/map_editor.js
+++ tools/map_editor.js
@@ -5,7 +5,6 @@ class CellData {
     }
 }
 
-const DEFAULT_SIZE = 48
 const ID_MAP = "map"
 const Modes = Object.freeze({
     TILES: Symbol("tiles"),
@@ -24,6 +23,7 @@ const ElementsDefinitions = Object.freeze([
     "GUARD_HOUSE",
     "ANTENNA",
 ]);
+const MAP_SIZES = Object.freeze({ small: 48, medium: 96, large: 192 });
 const TILE_SAND = Object.freeze({ tile: "#886A08", symbol: "0" })
 const TILE_ROAD = Object.freeze({ tile: "#424242", symbol: "1" });
 const body = document.body;
@@ -37,104 +37,129 @@ const RADIO_GROUP_NAME_GAME_OBJECT_SELECTIONS = "game_object_selections";
 const DIV_ID_BUTTON_SAVE = "button_save";
 const DIV_ID_BUTTON_LOAD = "button_load";
 const OUTPUT_FILE_NAME = 'map.json';
+const SELECT_ID_DROPDOWN_MAP_SIZES = "dropdown_map_sizes";
 class MapEditor {
 
     constructor() {
-        this.createTable();
+        this.resetMap();
         this.initializeModesRadioButtons();
         this.inflateElementsLeftMenu();
-        table.style.width = DEFAULT_SIZE * 64 + 'px';
-        table.style.height = DEFAULT_SIZE * 64 + 'px';
+        fillMapSizesDropdown();
+        table.style.width = this.map_size * 64 + 'px';
+        table.style.height = this.map_size * 64 + 'px';
         var self = this;
-        document.getElementById(DIV_ID_BUTTON_SAVE).addEventListener('click', e => {
-            var output = {};
-            var tilesString = calculateTilesMapString();
-            output.tiles = tilesString;
-            var elementsArray = calculateElementsArray();
-            output.elements = elementsArray;
-            var json = JSON.stringify(output);
-            saveJsonToFile(json);
+        defineSaveProcess();
+        defineLoadProcess();
 
-            function calculateElementsArray() {
-                var elementsArray = [];
-                for (var row in table.rows) {
-                    for (var col in table.rows[row].cells) {
-                        if (table.rows[row].cells[col].cellData != null) {
-                            if (table.rows[row].cells[col].cellData.object != null) {
-                                deflateElementObject(elementsArray, row, col);
+        function fillMapSizesDropdown() {
+            var dropDown = document.getElementById(SELECT_ID_DROPDOWN_MAP_SIZES);
+            for (var size in MAP_SIZES) {
+                var option = document.createElement("option");
+                option.value = size;
+                option.text = size;
+                dropDown.appendChild(option);
+            }
+            dropDown.addEventListener("change", function () {
+                self.resetMap(MAP_SIZES[dropDown.value]);
+            })
+        }
+
+        function defineLoadProcess() {
+            document.getElementById(DIV_ID_BUTTON_LOAD).addEventListener('click', () => {
+
+                var input = document.createElement('input');
+                input.type = 'file';
+                input.addEventListener('change', e => {
+                    var file = e.target.files[0];
+                    if (!file) {
+                        return;
+                    }
+                    var reader = new FileReader();
+                    reader.onload = e => {
+                        var contents = e.target.result;
+                        var inputMapObject = JSON.parse(contents);
+                        self.resetMap(inputMapObject.size);
+                        inflateTiles();
+                        for (var i = 0; i < inputMapObject.elements.length; i++) {
+                            var element = inputMapObject.elements[i];
+                            self.placeElementObject(table.rows[element.row].cells[element.col], element.definition);
+                        }
+
+                        function inflateTiles() {
+                            for (var i = 0; i < inputMapObject.tiles.length; i++) {
+                                var cell = table.rows[Math.floor(i / self.map_size)].cells[i % self.map_size];
+                                var placedTile = self.findTileBySymbol(inputMapObject.tiles.charAt(i));
+                                cell.style.backgroundColor = placedTile.tile;
+                                cell.cellData = new CellData();
+                                cell.cellData.tile = placedTile;
+                            }
+                        }
+                    };
+                    reader.readAsText(file);
+                }, false);
+                input.click();
+            });
+        }
+
+        function defineSaveProcess() {
+            document.getElementById(DIV_ID_BUTTON_SAVE).addEventListener('click', e => {
+                var output = {};
+                var tilesString = calculateTilesMapString();
+                output.tiles = tilesString;
+                var elementsArray = calculateElementsArray();
+                output.elements = elementsArray;
+                output.size = self.map_size;
+                var json = JSON.stringify(output);
+                saveJsonToFile(json);
+
+                function calculateElementsArray() {
+                    var elementsArray = [];
+                    for (var row in table.rows) {
+                        for (var col in table.rows[row].cells) {
+                            if (table.rows[row].cells[col].cellData != null) {
+                                if (table.rows[row].cells[col].cellData.object != null) {
+                                    deflateElementObject(elementsArray, row, col);
+                                }
                             }
                         }
                     }
-                }
-                return elementsArray;
+                    return elementsArray;
 
-                function deflateElementObject(elementsArray, row, col) {
-                    var elementObject = {};
-                    elementObject.definition = table.rows[row].cells[col].cellData.object;
-                    elementObject.row = parseInt(row);
-                    elementObject.col = parseInt(col);
-                    elementsArray.push(elementObject);
+                    function deflateElementObject(elementsArray, row, col) {
+                        var elementObject = {};
+                        elementObject.definition = table.rows[row].cells[col].cellData.object;
+                        elementObject.row = parseInt(row);
+                        elementObject.col = parseInt(col);
+                        elementsArray.push(elementObject);
+                    }
                 }
-            }
 
-            function calculateTilesMapString() {
-                var tilesString = "";
-                for (var row = 0; row < table.rows.length; row++) {
-                    for (var col = 0; col < table.rows[row].cells.length; col++) {
-                        var currentTile = TILE_SAND;
-                        var cellData = table.rows[row].cells[col].cellData;
-                        if (cellData != null && cellData.tile != null) {
-                            currentTile = self.findTileBySymbol(cellData.tile.symbol);
+                function calculateTilesMapString() {
+                    var tilesString = "";
+                    for (var row = 0; row < table.rows.length; row++) {
+                        for (var col = 0; col < table.rows[row].cells.length; col++) {
+                            var currentTile = TILE_SAND;
+                            var cellData = table.rows[row].cells[col].cellData;
+                            if (cellData != null && cellData.tile != null) {
+                                currentTile = self.findTileBySymbol(cellData.tile.symbol);
+                            }
+                            tilesString += currentTile.symbol;
                         }
-                        tilesString += currentTile.symbol;
                     }
+                    return tilesString;
                 }
-                return tilesString;
-            }
 
-            function saveJsonToFile(json) {
-                var bb = new Blob([json], { type: 'text/json' });
-                var a = document.createElement('a');
-                a.download = OUTPUT_FILE_NAME;
-                a.href = window.URL.createObjectURL(bb);
-                a.click();
-            }
-
-
-        });
-
-        document.getElementById(DIV_ID_BUTTON_LOAD).addEventListener('click', () => {
-
-            var input = document.createElement('input');
-            input.type = 'file';
-            input.addEventListener('change', e => {
-                var file = e.target.files[0];
-                if (!file) {
-                    return;
+                function saveJsonToFile(json) {
+                    var bb = new Blob([json], { type: 'text/json' });
+                    var a = document.createElement('a');
+                    a.download = OUTPUT_FILE_NAME;
+                    a.href = window.URL.createObjectURL(bb);
+                    a.click();
                 }
-                var reader = new FileReader();
-                var self = this;
-                reader.onload = e => {
-                    self.createTable();
-                    var contents = e.target.result;
-                    var inputMapObject = JSON.parse(contents);
-                    for (var i = 0; i < inputMapObject.tiles.length; i++) {
-                        var cell = table.rows[Math.floor(i / DEFAULT_SIZE)].cells[i % DEFAULT_SIZE];
-                        var placedTile = self.findTileBySymbol(inputMapObject.tiles.charAt(i));
-                        cell.style.backgroundColor = placedTile.tile;
-                        cell.cellData=new CellData();
-                        cell.cellData.tile = placedTile;
-                    }
-                    for (var i = 0; i < inputMapObject.elements.length; i++) {
-                        var element = inputMapObject.elements[i];
-                        self.placeElementObject(table.rows[element.row].cells[element.col], element.definition);
-                    }
-                };
-                reader.readAsText(file);
-            }, false);
-            input.click();
-        });
 
+
+            });
+        }
     }
 
     findTileBySymbol(symbol) {
@@ -247,11 +272,13 @@ class MapEditor {
         return textNode
     }
 
-    createTable() {
+    resetMap(map_size = MAP_SIZES.small) {
         table.innerHTML = "";
-        for (let i = 0; i < DEFAULT_SIZE; i++) {
+        this.map_size = map_size;
+        document.getElementById(SELECT_ID_DROPDOWN_MAP_SIZES).value = findMapSizeDefinitionByValue(this.map_size);
+        for (let i = 0; i < this.map_size; i++) {
             const tr = table.insertRow();
-            for (let j = 0; j < DEFAULT_SIZE; j++) {
+            for (let j = 0; j < this.map_size; j++) {
                 const td = tr.insertCell();
                 td.addEventListener('click', e => {
                     this.onCellLeftClicked(i, j);
@@ -262,6 +289,16 @@ class MapEditor {
                     return false;
                 }, false)
             }
+        }
+
+        function findMapSizeDefinitionByValue(value) {
+            var result = null;
+            for (const [key, sizeValue] of Object.entries(MAP_SIZES)) {
+                if (sizeValue == value) {
+                    result = key;
+                }
+            }
+            return result;
         }
     }
 
